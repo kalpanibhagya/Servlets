110a3261d8f4fee8d8e0d37c812b094b2565be01
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2017-03-05 06:51:28 +0530

    JDBC connectivity in Eclipse IDE
    This is an example for how to create a web application with JDBC connectivity in Eclipse IDE using JavaEE. Here when a user goes to the login.html  page and enter his username and password, it checks whether the user details in the database. If the details are already in the database it redirects to the home.html page. If not, it redirects to the registration.html page and let the user to insert the details about him/her. After the registration process, it goes to the insert_message.html page and says "Successfully inserted".

---
+++ second/registerusers/WEB-INF/classes/pk1/RegisterServlet.java
@@ -0,0 +1,57 @@
+package pk1;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public String DB_URL = "jdbc:mysql://localhost:3306/registration";
+	public String DB_USER = "root";
+	public String DB_PWD = "";
+    
+    public RegisterServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String full_name = request.getParameter("fullname");
+		String city = request.getParameter("city");
+		String sex = request.getParameter("gender");
+		String email= request.getParameter("email");
+		String tele = request.getParameter("phone");
+		
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			Connection conn = DriverManager.getConnection(DB_URL,DB_USER,DB_PWD);
+			Statement stmt =conn.createStatement();
+			
+			String insert = "INSERT INTO reg_user (user_name,password,full_name,city,sex,email,tele) VALUES ('"+username+"','"+password+"','"+full_name+"','"+city+"','"+sex+"','"+email+"','"+tele+"')";
+			stmt.executeUpdate(insert);
+			
+			response.sendRedirect("insert_message.html");
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}catch(SQLException sql){
+			sql.printStackTrace();
+		}
+	}
+
+}
